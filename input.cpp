f10b12835f1b1e10aa9c9eaa01d1d98fc5b8d4fb
Jonas Greitemann <jgreitemann@gmail.com>
2021-12-03 00:19:19 +0100

    Switch from cpr/libcurl to httplib for downloading puzzle inputs

--- input.cpp
+++ input.cpp
@@ -1,5 +1,6 @@
 #include "input.h"
-#include <cpr/cpr.h>
+#include "httplib.h"
+
 #include <fmt/core.h>
 
 #include <filesystem>
@@ -11,33 +12,39 @@ namespace AoC {
     static const auto session =
             std::string{std::getenv("SESSION")};
 
-    auto input_tmp_path(int day) -> std::filesystem::path
-    {
+    auto input_tmp_path(int day) -> std::filesystem::path {
       return std::filesystem::temp_directory_path() / fmt::format("aoc21-day{:02}.txt", day);
     }
 
-    auto download_input(int day) -> std::string
-    {
-      cpr::Response r = cpr::Get(cpr::Url{fmt::format("https://adventofcode.com/2020/day/{}/input", day)},
-                                 cpr::Cookies{{"session", session}});
-      if (r.status_code == 200) {
-        return r.text;
+    auto download_input(int day) -> std::string {
+      httplib::Client client{"https://adventofcode.com"};
+      client.enable_server_certificate_verification(false);
+
+      httplib::Headers cookie{{"Cookie", fmt::format("session={}", session)}};
+      if (auto res = client.Get(fmt::format("/2021/day/{}/input", day).c_str(), cookie)) {
+        if (res->status == 200) {
+          return res->body;
+        } else {
+          throw std::runtime_error{fmt::format("Failed to download puzzle input: Status {}", res->status)};
+        }
       } else {
-        throw std::runtime_error{fmt::format("Failed to download puzzle input: Status {}", r.status_code)};
+        throw std::runtime_error{"Failed to download puzzle input: Error sending GET request."};
       }
     }
-  }
 
-  auto input_stream(int day) -> std::ifstream
-  {
+  }// namespace
+
+  auto input_stream(int day) -> std::ifstream {
     auto path = input_tmp_path(day);
 
     if (!std::filesystem::is_regular_file(path)) {
-      std::ofstream os{path};
-      os << download_input(day);
+      if (auto downloaded_input = download_input(day); !downloaded_input.empty()) {
+        std::ofstream os{path};
+        os << download_input(day);
+      }
     }
 
     return std::ifstream{path};
   }
 
-}
+}// namespace AoC
